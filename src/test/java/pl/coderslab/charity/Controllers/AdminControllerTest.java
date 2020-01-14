e1a4abf20a01900f012b86bf160edb09ba5f1ac6
PawelGrad <gradpawel91@gmail.com>
2020-01-14 10:59:26 +0100

    fixes

---
+++ src/test/java/pl/coderslab/charity/Controllers/AdminControllerTest.java
@@ -0,0 +1,24 @@
+package pl.coderslab.charity.Controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdminControllerTest {
+
+    @Test
+    public void mainPage() {
+    }
+
+    @Test
+    public void users() {
+    }
+
+    @Test
+    public void removeUser() {
+    }
+
+    @Test
+    public void blockUser() {
+    }
+}
